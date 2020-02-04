902048d2a16d473cb1a724a0b3d5b69d39540e76
Robert Ignat <robert.ignat91@gmail.com>
2020-02-04 11:38:29 +0200

    Upgrade tungstenite
    * replace async_tungstenite with tokio_tungstenite

--- src/server/ws.rs
+++ src/server/ws.rs
@@ -1,5 +1,5 @@
-use async_tungstenite::{tokio::TokioAdapter, WebSocketStream};
 use hyper::{upgrade::Upgraded, Body, Request};
+use tokio_tungstenite::WebSocketStream;
 use tungstenite::protocol::Role;
 
 mod handshake;
@@ -7,13 +7,9 @@ mod handshake;
 pub use handshake::handshake;
 
 /// Use AFTER SUCCESSFUL HANDSHAKE to upgrade Request to WebSocket.
-pub async fn upgrade(
-    req: Request<Body>,
-) -> Result<WebSocketStream<TokioAdapter<Upgraded>>, hyper::Error> {
+pub async fn upgrade(req: Request<Body>) -> Result<WebSocketStream<Upgraded>, hyper::Error> {
     match req.into_body().on_upgrade().await {
-        Ok(upgraded) => {
-            Ok(WebSocketStream::from_raw_socket(TokioAdapter(upgraded), Role::Server, None).await)
-        }
+        Ok(upgraded) => Ok(WebSocketStream::from_raw_socket(upgraded, Role::Server, None).await),
         Err(e) => Err(e),
     }
 }

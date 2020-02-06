68ba038c62a59573d1f79d7793131dbd8e8c48f8
Robert Ignat <robert.ignat91@gmail.com>
2020-02-06 12:15:34 +0200

    Use EventObjParser to forward events to kafka

--- src/event.rs
+++ src/event.rs
@@ -1,54 +1,20 @@
 use bytes::Bytes;
-use rdkafka::{error::KafkaResult, producer::FutureProducer};
+use futures::future::try_join_all;
+use rdkafka::producer::FutureProducer;
 
 use crate::{config::Kafka, error::Result, kafka};
 
 mod parser;
 
+use parser::EventObjParser;
+
 /// Parses and forwards events to a kafka producer.
 /// Events are JSON objects.
 /// Data should be an array of events.
 pub async fn forward_to_kafka(buf: Bytes, producer: &FutureProducer, config: &Kafka) -> Result<()> {
-    // check that body is a valid JSON array
-    let json_data = json::parse(std::str::from_utf8(buf.as_ref())?)?;
-    if !json_data.is_array() {
-        Err(json::Error::wrong_type("array"))?;
-    }
+    let parser = EventObjParser::new(buf)?;
 
-    let mut obj_start = 0;
-    let mut braces = 0;
-    let mut send_results = Vec::with_capacity(json_data.len());
-
-    for i in 0..buf.len() {
-        match buf.get(i) {
-            // this should never happen
-            None => break,
-
-            Some(b'{') => {
-                if braces == 0 {
-                    obj_start = i;
-                }
-
-                braces += 1;
-            }
-
-            Some(b'}') => {
-                braces -= 1;
-
-                if braces == 0 {
-                    // this is the end of an object
-                    let obj = buf.slice(obj_start..=i);
-
-                    send_results.push(kafka::send(producer, obj, config).await);
-                }
-            }
-
-            // do nothing for all other chars
-            _ => {}
-        }
-    }
-
-    send_results.into_iter().collect::<KafkaResult<_>>()?;
+    try_join_all(parser.map(|event| kafka::send(producer, event, config))).await?;
 
     Ok(())
 }

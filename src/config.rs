379c795335073257e26318264faaf008be026379
Robert Ignat <robert.ignat91@gmail.com>
2020-02-11 11:15:54 +0200

    Add JSON logging config option

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,7 @@ use std::net::SocketAddr;
 #[derive(Clone, Debug)]
 pub struct Config {
     pub addr: SocketAddr,
+    pub log_json: bool,
     pub kafka: Kafka,
 }
 
@@ -18,6 +19,7 @@ impl Default for Config {
     fn default() -> Config {
         Config {
             addr: ([127, 0, 0, 1], 8088).into(),
+            log_json: false,
             kafka: Kafka::default(),
         }
     }
